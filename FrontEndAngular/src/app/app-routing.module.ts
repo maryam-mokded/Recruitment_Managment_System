685fd611a21187630f2d17014661345dd090a79d
hanin303 <benjemaahanin@gmail.com>
2021-11-06 23:08:55 +0100

    Hanin_Routing_Generating

--- FrontEndAngular/src/app/app-routing.module.ts
+++ FrontEndAngular/src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { AccueilComponent } from './accueil/accueil.component';
+import { ContactComponent } from './contact/contact.component';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes :Routes =[
-  {path:'accueil',component:AccueilComponent}
+  {path:'accueil',component:AccueilComponent},
+  {path:'contact',component:ContactComponent}
 ]
 
 @NgModule({
