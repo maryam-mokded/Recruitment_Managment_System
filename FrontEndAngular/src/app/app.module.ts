356987bbac5482e1cde4b5a6a46aeb450735025b
hanin303 <benjemaahanin@gmail.com>
2021-11-06 09:12:13 +0100

    Hanin_Angular_Material_Integration

--- FrontEndAngular/src/app/app.module.ts
+++ FrontEndAngular/src/app/app.module.ts
@@ -2,13 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSliderModule } from '@angular/material/slider';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatSliderModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
