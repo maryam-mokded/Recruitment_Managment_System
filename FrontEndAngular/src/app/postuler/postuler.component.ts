77b2baa7a66cf3c066bf2dd4bdc4ab3bc8a35802
Mokded Maryam <80172649+maryam-mokded@users.noreply.github.com>
2021-12-04 11:52:19 +0100

    second commit

--- FrontEndAngular/src/app/postuler/postuler.component.ts
+++ FrontEndAngular/src/app/postuler/postuler.component.ts
@@ -1,6 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { MatDialog,MatDialogConfig } from '@angular/material/dialog';
+import { Offers } from '../Models/offers';
+import { OffresService } from '../Services/offers.service';
+import { OffreDetailsComponent } from '../offre-details/offre-details.component';
+import {
 
+} from '@angular/forms';
 @Component({
   selector: 'app-postuler',
   templateUrl: './postuler.component.html',
@@ -8,22 +13,41 @@ import { MatDialog,MatDialogConfig } from '@angular/material/dialog';
 })
 export class PostulerComponent implements OnInit {
 
-  valeurs: string = '';
+  public OneOffer? : Offers;
 
 
-  constructor(private dialogClose :MatDialog) { }
+  constructor(
+    private dialog :MatDialog,
+    private dialogClose :MatDialog,
+    private offresServ : OffresService,
+    ) { }
 
   ngOnInit(): void {
+    //Revenir a offre détails
+    this.offresServ.ConsulterOffer(JSON.parse(localStorage.getItem('IdOffer') || '[]') || []).subscribe(o =>{
+      this.OneOffer = o;
+      console.log(this.OneOffer);
+     });
+
+
+
+
+
   }
+
+
   onClose(){
     this.dialogClose.closeAll();
   }
 
-  save(){
-    console.log(this.valeurs);
-
+  onRevenir(Offer : Offers){
+    this.dialogClose.closeAll();
+    const dialogConfig = new MatDialogConfig();
+    dialogConfig.disableClose = true;
+    dialogConfig.autoFocus = true;
+    localStorage.setItem('IdOffer', JSON.stringify(Offer.idOffre));
+    this.dialog.open(OffreDetailsComponent, dialogConfig);
   }
-
   // onkey(event:any){
   //   // (keyup)="onkey($event)"
   //   // this.value = event.target.value;
