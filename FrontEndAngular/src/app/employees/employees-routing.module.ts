f271f6efaca2e4796d28130cf2606adb19313d62
Mokded Maryam <80172649+maryam-mokded@users.noreply.github.com>
2021-12-29 02:50:15 +0100

    Maryam_Add_Routing_SousModule

--- FrontEndAngular/src/app/employees/employees-routing.module.ts
+++ FrontEndAngular/src/app/employees/employees-routing.module.ts
@@ -4,17 +4,19 @@ import { EmployeesComponent } from './employees.component';
 import { AdminComponent } from './admin/admin.component';
 import { RecruteurComponent } from './recruteur/recruteur.component';
 import { InterviewerComponent } from './interviewer/interviewer.component';
+import { NotFoundComponent } from '../not-found/not-found.component';
 
 const routes: Routes = [
-  { path: '', component: EmployeesComponent },
-  { path: 'recruteur', component:RecruteurComponent  },
-  { path: 'admin', loadChildren: () => import('./admin/admin.module').then(m => m.AdminModule) },
-  { path: 'recruteur', loadChildren: () => import('./recruteur/recruteur.module').then(m => m.RecruteurModule) },
-  { path: 'interviewer', loadChildren: () => import('./interviewer/interviewer.module').then(m => m.InterviewerModule) },
-  { path: 'user-profil', loadChildren: () => import('./user-profil/user-profil.module').then(m => m.UserProfilModule) }];
-
-
-  
+  {
+    path: '', component: EmployeesComponent, children: [
+      { path: 'recruteur',loadChildren: () => import('./recruteur/recruteur.module').then(m => m.RecruteurModule)},
+      { path: 'admin', loadChildren: () => import('./admin/admin.module').then(m => m.AdminModule) },
+      { path: 'interviewer', loadChildren: () => import('./interviewer/interviewer.module').then(m => m.InterviewerModule) },
+      { path: 'user-profil', loadChildren: () => import('./user-profil/user-profil.module').then(m => m.UserProfilModule) },
+      { path: '**', component: NotFoundComponent},
+    ]
+  }
+];
 
 
 
